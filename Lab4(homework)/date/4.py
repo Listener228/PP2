df894800e863129bb2b39b0bdc77b1da8ce5336b
Bexultan <beksultanshokey2006@gmail.com>
2025-02-19 22:24:33 +0500

    Hw

--- Lab4(homework)/date/4.py
+++ Lab4(homework)/date/4.py
@@ -1,7 +1,16 @@
 import datetime
 
-x = datetime.datetime.now()
-x1 = x.second + 20
-difference = x1 - x.second
+days = int(input())
 
-print(difference)
+def days_to_seconds(days):
+    """Convert days into seconds."""
+    return days * 24 * 60 * 60
+
+x = datetime.datetime.now()
+x1 = x + datetime.timedelta(days)
+
+difference_in_days = (x1 - x).days
+
+difference_in_seconds = days_to_seconds(difference_in_days)
+
+print(f"Difference in seconds: {difference_in_seconds}")
